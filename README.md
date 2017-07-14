7c19e8208dfabceee8486103c47393c2f1c3cec8
Justin Albert <jalbert@heplw53.phys.uvic.ca>
2017-07-13 19:10:51 -0700

    initial upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# AIFCOMSSwithCUPredictorTest
-Testing area for AIFCOMSS when integrated together with the CUSF balloon flight prediction code
---
+++ pred_src/CMakeFiles/pred.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/altitude.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/altitude.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/ini/dictionary.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/ini/dictionary.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/ini/iniparser.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/ini/iniparser.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/pred.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/pred.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/run_model.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/run_model.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/util/getdelim.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/util/getdelim.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/util/getline.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/util/getline.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/util/gopt.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/util/gopt.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/util/random.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/util/random.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/wind/wind_file.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/wind/wind_file.o"
+  "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/wind/wind_file_cache.c" "/hepuser/jalbert/cusf-standalone-predictor-master/pred_src/CMakeFiles/pred.dir/wind/wind_file_cache.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
